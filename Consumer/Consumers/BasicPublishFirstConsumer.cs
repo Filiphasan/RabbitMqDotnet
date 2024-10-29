08edb632a2576c56c5a49b8462d8918898e70afd
Hasan Erdal <hasaerda@hotmail.com>
2024-10-29 21:28:58 +0300

    .

---
+++ Consumer/Consumers/BasicPublishFirstConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Consumer.Services.Interfaces;
+using Shared.Common.Constants;
+using Shared.Common.QueueModels;
+using Shared.Services;
+
+namespace Consumer.Consumers;
+
+public class BasicPublishFirstConsumer : BaseConsumer<QueueBasicModel>
+{
+    public BasicPublishFirstConsumer(RabbitMqConnectionService rabbitMqConnectionService, ILoggerFactory loggerFactory, IServiceScopeFactory scopeFactory) : base(rabbitMqConnectionService, loggerFactory, scopeFactory)
+    {
+    }
+
+    protected override async Task ConsumeAsync(QueueBasicModel message)
+    {
+        var scope = ScopeFactory.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<IMyService>();
+        await service.DoBasicPublishFirstConsumerWorkAsync(message);
+        scope.Dispose();
+    }
+
+    protected override void SetupConsumer()
+    {
+        QueueName = QueueConstant.QueueNames.BasicPublishFirstQueue;
+        ExchangeName = QueueConstant.ExchangeNames.BasicPublishExchange;
+        RoutingKey = QueueConstant.RoutingKeys.BasicPublishRoutingKey;
+        MaxRetryCount = 2;
+        RetryDelayMs = 60_000;
+    }
+}
---
+++ Publisher/Models/BasicPublishRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Publisher.Models;
+
+public class BasicPublishRequest
+{
+    public int PickedNumber { get; set; }
+    public string? Message { get; set; }
+}
