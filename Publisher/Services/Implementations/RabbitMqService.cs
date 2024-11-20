a3d7ceecce7ede82b504f0caaaa6b1fcaaae9efe
Hasan Erdal <hasaerda@hotmail.com>
2024-11-21 00:26:10 +0300

    .

--- Publisher/Services/Implementations/RabbitMqService.cs
+++ Publisher/Services/Implementations/RabbitMqService.cs
@@ -9,29 +9,6 @@ namespace Publisher.Services.Implementations;
 
 public class RabbitMqService(RabbitMqConnectionService rabbitMqConnectionService) : IRabbitMqService
 {
-    public async Task SendAsync<T>(T message, string queueName, string messageId , CancellationToken cancellationToken = default)
-    {
-        await using var channel = await rabbitMqConnectionService.GetChannelAsync();
-        var properties = new BasicProperties
-        {
-            Headers = new Dictionary<string, object?> { { "x-message-id", messageId } },
-            Persistent = true,
-            Priority = 0,
-            DeliveryMode = DeliveryModes.Persistent
-        };
-        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
-        await channel.BasicPublishAsync("", queueName, false, properties, body, cancellationToken);
-    }
-
-    public async Task PublishAsync<T>(T message, string exchangeName, string routingKey, string exchangeType = ExchangeType.Direct, CancellationToken cancellationToken = default)
-    {
-        await using var channel = await rabbitMqConnectionService.GetChannelAsync();
-        await channel.ExchangeDeclareAsync(exchangeName, exchangeType, cancellationToken: cancellationToken);
-
-        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
-        await channel.BasicPublishAsync(exchangeName, routingKey, false, body, cancellationToken: cancellationToken);
-    }
-
     public async Task SendAsync<T>(SendMessageModel<T> message, CancellationToken cancellationToken = default) where T : class
     {
         var channel = await rabbitMqConnectionService.GetChannelAsync();
